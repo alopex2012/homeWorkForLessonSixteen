bb5677055b184d65ce66c6cb5eb2db45a953bcb5
Serhii <serhii.lohachov@gmail.com>
2022-11-30 03:03:25 +0200

    Added classes

--- src/Circle.php
+++ src/Circle.php
@@ -1,1 +1,47 @@
 <?php
+
+namespace App;
+/**
+ * Class Circle
+ * @package App
+ */
+class Circle
+{
+    /**
+     * Constant, expressing the ratio of the circumference of a circle to the length of its diameter
+     */
+    const PI = 3.1415926535;
+
+    /**
+     * @var int
+     */
+    private int $radius;
+
+    /**
+     * Circle constructor.
+     * @param $radius
+     */
+    public function __construct($radius)
+    {
+        $this->radius = $radius;
+    }
+
+    /**
+     * Method returns area of circle
+     * @return float
+     */
+    public function getSquare(): float
+    {
+        return round(self::PI * $this->radius ** 2, 2);
+    }
+
+    /**
+     * Method returns circumference
+     * @return float
+     */
+    public function getCircuit(): float
+    {
+        return round(2 * self::PI * $this->radius, 2);
+    }
+
+}
--- src/User.php
+++ src/User.php
@@ -1,1 +1,39 @@
 <?php
+
+namespace App;
+/**
+ * Class User
+ * @package App
+ */
+class User
+{
+    /**
+     * @var int
+     */
+    private static int $count = 0;
+
+    /**
+     * @var string
+     */
+    public string $name;
+
+    /**
+     * User constructor.
+     * @param string $name
+     */
+    public function __construct(string $name)
+    {
+        $this->name = $name;
+        self::$count++;
+    }
+
+    /**
+     * Method returns number of class objects created
+     * @return int
+     */
+    public static function getCount(): int
+    {
+        return self:: $count;
+    }
+
+}
